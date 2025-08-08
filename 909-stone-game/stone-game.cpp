d2cecc6036802b2e0bc3dc21e00b094766329d7a
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-08-08 10:04:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.00%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-    int n = piles.size();
-    
-    sort(piles.begin() , piles.end(),greater<int>()); //sort in decending order
-    int Alice = 0;
-    int bob  =0;
-
-    for(int i =0 ;i<n;i++){
-           if(i%2 == 0){
-              Alice+=piles[i];
-           }
-           if(i%2 != 0) bob+=piles[i];
-    }
-
-if(Alice > bob) return true; //alice win the game
-return false;
+       
+       return true;
 
     }
 };
