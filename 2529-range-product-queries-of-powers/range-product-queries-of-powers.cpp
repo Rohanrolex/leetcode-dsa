36a2c64bce6ff2450d581c0f8ccab511c5070a04
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-08-11 21:36:20 +0530

    Time: 15 ms (75.53%) | Memory: 147.5 MB (69.68%) - LeetSync

---
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+   const int MOD = 1e9 + 7;
+
+
+   vector<int>power;
+
+
+
+   for(int i =0 ;i<31;i++){
+      if(n & (1<<i)){
+         power.push_back(1<<i);
+      }
+   }
+
+
+   vector<int>ans;
+
+      for(auto &p : queries){
+
+         long long  product = 1;
+
+         for(int i = p[0] ;i<=p[1];i++){
+            product = (product * power[i])%MOD;
+
+
+         }
+
+         ans.push_back(int(product));
+      }
+
+
+
+return ans;
+    }
+};
