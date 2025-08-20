9cbf65a8be19173574df539b09659adf71578839
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-08-20 16:30:47 +0530

    Time: 11 ms (28.49%) | Memory: 30.3 MB (70.36%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    int countSquares(vector<vector<int>>& matrix) {
+
+int countans(vector<vector<int>>& dp , vector<vector<int>>& matrix){
         int n = matrix.size();
-        int m = matrix[0].size();
+         int m = matrix[0].size();
+         int total =0;
+  
+         for(int i =0 ;i<n ;i++){
+             for (int j =0 ;j<m ;j++){
 
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-        int totalCount = 0;
+                  if(matrix[i][j] == 1){
+                    if(i ==0  || j ==0) dp[i][j] =1;
+                      
+                      else{
+                           dp[i][j] = 1+ min({dp[i][j-1] ,dp[i-1][j] , dp[i-1][j-1]});
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 1) {
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1;
-                    } else {
-                        dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
-                    }
-                    totalCount += dp[i][j];
-                }
-                
-            }
-        }
+                      }
+                      total+= dp[i][j];
+                  }
 
-        return totalCount;
+             }
+         }
+       return total;
+}
+    int countSquares(vector<vector<int>>& matrix) {
+        
+         int n = matrix.size();
+         int m = matrix[0].size();
+
+         vector<vector<int>>dp(n , vector<int>(m,0));
+
+         return countans(dp , matrix);
     }
 };
