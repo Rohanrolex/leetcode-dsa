1ab926f3e02bc5f3f75464a61636efa470fa98ac
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-09-11 10:07:19 +0530

    Time: 19 ms (35.71%) | Memory: 15.6 MB (92.02%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+       
+        int n =s.size();
+     string p ="";
+
+     for(int i =0 ;i<n ;i++){
+         if (s[i] == 'a' ||s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+        s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+            p.push_back(s[i]);
+
+        } 
+     }
+
+     sort(p.begin(), p.end());
+
+
+     int x =0;//point into the p string
+     while(x<p.size()){
+
+
+
+        for(int i =0 ;i<n ;i++){
+         if (s[i] == 'a' ||s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+        s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+             s[i] = p[x];
+             x++; //increase the value of x pointer
+
+        } 
+     }
+
+
+     }
+
+     return s;
+
+    }
+};
