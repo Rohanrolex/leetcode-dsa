a3c0a9c4c7eb37bc4384ddb2f20c2ad180f49c2f
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-09-11 10:11:07 +0530

    Time: 14 ms (60.26%) | Memory: 15.7 MB (78.32%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -8,7 +8,7 @@ public:
      for(int i =0 ;i<n ;i++){
          if (s[i] == 'a' ||s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
         s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
-            p.push_back(s[i]);
+          p.push_back(s[i]); //push only vowel 
 
         } 
      }
