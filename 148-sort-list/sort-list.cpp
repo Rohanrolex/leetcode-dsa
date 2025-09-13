0f7b12fd527db8913ffcf885fca8e30b765384cf
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-09-13 11:28:29 +0530

    Time: 55 ms (18.00%) | Memory: 75.6 MB (45.79%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,22 +10,54 @@
  */
 class Solution {
 public:
+
+ListNode* marge(ListNode* left , ListNode* right){
+    ListNode* dummy = new ListNode(-1);
+    ListNode* temp = dummy;
+
+    while(left != NULL && right != NULL){
+        if(left->val <= right->val){
+            temp->next = left;
+            left = left->next;
+        }else{
+            temp->next = right;
+            right = right->next;
+        }
+         temp = temp->next;
+    }
+
+    if(left !=NULL){
+        temp->next =left;
+    }
+
+    if(right !=NULL){
+        temp->next =right;
+    }
+    return dummy->next;
+   
+}
     ListNode* sortList(ListNode* head) {
-         vector<int>nums;
-         
-       ListNode*temp = head;
-         while(temp != NULL){
-            nums.push_back(temp->val);
-            temp = temp->next;
-         }
-         sort(nums.begin() , nums.end());
+        if(head == NULL || head->next == NULL)  return head;
 
-         temp = head;
 
-         for(int i =0 ;i<nums.size();i++){
-             temp->val = nums[i];
-             temp = temp->next;
-         }
-     return head;
+        ListNode* fast = head;
+       ListNode* slow = head;
+        ListNode* prev = NULL;
+
+        while(fast != NULL && fast->next!= NULL){
+             prev =slow;
+             slow = slow->next;
+             fast = fast->next->next;
+        }
+ //devide into two parts
+        if(prev != NULL){
+            prev->next = NULL;
+        }
+
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(slow);
+
+return marge(left , right);
+
     }
 };
