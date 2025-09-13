8ef835434dcdf544f2d20129ee5861b71ca0154b
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-09-13 11:48:47 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (91.71%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,31 +1,40 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+    // reverse linked list
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-       ListNode* fast = head;
-       ListNode* slow =head;
-       for(int i =0 ;i<n ;i++){
-        fast =fast->next;
-       }
-       if(fast==NULL) return head->next;
+        if (!head) return nullptr;
 
-       while(fast->next!=NULL){
-          fast = fast->next;
-          slow = slow->next;
+        //  Reverse the list
+        head = reverseList(head);
 
-       }
-       ListNode* delNode = slow->next;
-       slow->next = slow->next->next;
-       delete delNode;
-       return  head;   
+        //  Remove nth node from start
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* curr = dummy;
+
+        for (int i = 1; i < n; i++) {
+            if (curr->next) curr = curr->next;
+        }
+
+        if (curr->next) {
+            curr->next = curr->next->next;
+        }
+
+        //  Reverse again to restore original order
+        head = reverseList(dummy->next);
+
+        return head;
     }
 };
