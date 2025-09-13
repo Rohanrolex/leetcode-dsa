abbff3fa4d784d8c2de1d33cd736703bba60b5cb
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-09-13 12:03:07 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (91.71%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-    // reverse linked list
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        while (curr) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
+  
 
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         if (!head) return nullptr;
+    ListNode* dummy = new ListNode(0);  
+     dummy->next = head;
+    ListNode* fast = dummy;
+    ListNode* slow = dummy;
+ 
 
-        //  Reverse the list
-        head = reverseList(head);
+    for(int i =0 ;i<=n;i++){
+        fast = fast->next;
+    }
 
-        //  Remove nth node from start
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        ListNode* curr = dummy;
-
-        for (int i = 1; i < n; i++) {
-            if (curr->next) curr = curr->next;
-        }
-
-        if (curr->next) {
-            curr->next = curr->next->next;
-        }
-
-        //  Reverse again to restore original order
-        head = reverseList(dummy->next);
-
-        return head;
+    while(fast != NULL){
+       fast = fast->next;
+       slow = slow->next;
+    }
+    slow->next = slow->next->next;
+       return dummy->next;
     }
 };
