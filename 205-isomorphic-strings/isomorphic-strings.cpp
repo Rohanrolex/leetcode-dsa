97bbbf1460b7c221eb1c9815e2b3d7da1847402e
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-09-16 10:18:54 +0530

    Time: 2 ms (52.77%) | Memory: 9.3 MB (56.67%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-       if(s.size()!=t.size()) return false;
+     if(s.size() != t.size()) return false;
 
-       vector<int>smap(256,-1);
-       vector<int>tmap(256,-1);
+     vector<int>smap(256,-1);
+     vector<int>tmap(256,-1);
 
-       for(int i =0 ;i<s.size();i++){
-          char c1 = s[i];
-          char c2 = t[i];
+     for(int i =0 ;i<s.size() ;i++){
+        char c1 = s[i];
+        char c2 = t[i];
 
-          if(smap[c1] == -1) smap[c1] = c2; //c1 point to c2
-          else if(smap[c1] != c2) return false;
+        if(smap[c1] == -1) smap[c1] = c2;
+        else if(smap[c1] != c2) return false;
 
-
-          if(tmap[c2] == -1) tmap[c2] = c1;
-          else if(tmap[c2] != c1) return false;
-       }
-       return true;
+        if(tmap[c2] == -1) tmap[c2] = c1;
+        else if(tmap[c2] != c1) return false;
+     }
+     return true;
     }
 };
