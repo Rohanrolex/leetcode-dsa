9a506754480ec1f5e7f917d20fadb5531e15155e
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-09-19 16:06:26 +0530

    Time: 37 ms (70.99%) | Memory: 53.5 MB (86.06%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // helper to check palindrome
+    bool isPalindrome(const string &p) {
+        int left = 0, right = p.size() - 1;
+        while (left < right) {
+            if (p[left] != p[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    // recursive function
+    void func(string s, vector<vector<string>> &ans, vector<string> &v) {
+        if (s.empty()) {
+            ans.push_back(v);
+            return;
+        }
+
+        for (int len = 1; len <= s.size(); len++) {
+            string x = s.substr(0, len);
+            if (isPalindrome(x)) {
+                v.push_back(x);
+                string y = s.substr(len);   // remainder of the string
+                func(y, ans, v);
+                v.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> v;
+        func(s, ans, v);
+        return ans;
+    }
+};
