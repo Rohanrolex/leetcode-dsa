e30da4b6163f14c0988b2dea1118f123848e2873
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-09-27 18:35:01 +0530

    Time: 2 ms (75.79%) | Memory: 11.8 MB (73.60%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
  int dp[301];
-  int helper(int i , string s , unordered_set<string>& st){
-     if(i == s.size()) return 1; //because it going to till end
-      if(dp[i] != -1) return dp[i];
-      string temp;
-     for(int j = i ;j<s.size();j++){
-          temp += s[j];
-          if(st.find(temp) != st.end()){
-             if(helper(j+1 ,s,st)) return dp[i] =  1; //match find
-          }
+
+ int ans(int i , string &s , unordered_set<string> &c){
+     if(i == s.size()) return 1; // all word present in the wordict
+     if(dp[i] != -1)return dp[i];
+ string temp; 
+     for(int j =i ;j<s.size();j++){
+       temp += s[j];
+
+       if(c.find(temp) != c.end()){
+          if(ans(j+1 , s, c)) return 1; //found the word
+       }   
      }
-     return  dp[i] = 0;
-  }
+    return  dp[i] = 0;
+ }
+  
     bool wordBreak(string s, vector<string>& wordDict) {
-        
-          unordered_set<string>st;
-          memset(dp , -1 , sizeof dp);
+        unordered_set<string>p;
+        memset(dp , -1 , sizeof dp);
+        for(auto it : wordDict){
+             p.insert(it);
+        }
+        return ans(0, s , p);
+
              
-             for(auto it : wordDict){
-                 st.insert(it);
-             }
-          return helper(0 , s , st);
+             
     }
 };
