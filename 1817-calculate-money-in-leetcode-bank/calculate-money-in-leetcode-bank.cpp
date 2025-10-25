0efafc438145e9e06b349266a04a4c7017c673d3
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-10-25 13:57:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.15%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -8,7 +8,7 @@ public:
         int incomplete = n%7;
 
         int part1 = (28*complete)+((complete*(complete-1))/2)*7;
-        int part2 = (incomplete*(incomplete+1))/2+ (complete)*incomplete;
+        int part2 = (incomplete*(incomplete+1))/2 + (complete)*incomplete;
 
         return part1+part2;
     }
