66fd20b5ff9ec6d899009b1fc100c69381a4c24f
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2025-10-31 08:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 25.4 MB (75.61%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-         vector<int>ans;
-         int n = nums.size();
-         unordered_map<int ,int>mpp(n);
-
-         for(auto it : nums){
-            mpp[it]++;
-         }
-
-         for(auto it : mpp){
-            if(it.second >= 2){
-                ans.push_back(it.first);
+        int n = nums.size();
+        vector<bool> seen(n, false);
+        vector<int> res;
+        for (int i = 0; i < n; i++) {
+            if (seen[nums[i]]) {
+                res.push_back(nums[i]);
+            } else {
+                seen[nums[i]] = true;
             }
-         }
-return ans;
+        }
+        if (res.size() == 2 && res[0] > res[1]) {
+            swap(res[0], res[1]);
+        }
+        return res;
     }
 };
