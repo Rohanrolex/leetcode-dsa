262af693d6676ec5773828f38686e72c9bdb76eb
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2026-01-28 10:01:25 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (76.05%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,15 +4,15 @@ public:
          int n = nums.size();
          int low =0;
          int high = n-1;
-        //if found
+
          while(low<=high){
-             int mid = low+(high-low)/2;
-           
-           if(nums[mid] == target) return mid;
-           if(nums[mid] < target) low = mid+1;
-           else high = mid-1;
+             int mid = (low+high)/2;
+
+             if(nums[mid] == target) return mid;
+
+             else if(nums[mid] > target) high =mid-1;
+             else low = mid+1;
          }
-         //if not found
          return low;
     }
 };
