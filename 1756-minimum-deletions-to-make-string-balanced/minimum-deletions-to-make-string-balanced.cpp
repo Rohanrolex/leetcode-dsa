7ac54beca7a2951227d95875f7cff5b312b094e5
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2026-02-07 23:29:35 +0530

    Time: 27 ms (56.07%) | Memory: 25.6 MB (72.34%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n = s.size();
+        int a = 0, b = 0;
+        int res = n;
+
+        // count total 'a'
+        for (char c : s) {
+            if (c == 'a') a++;
+        }
+
+        // try every split
+        for (char c : s) {
+            if (c == 'a') a--;          // remove from right
+            res = min(res, a + b);      // deletions needed
+            if (c == 'b') b++;          // add to left
+        }
+
+        return res;
+    }
+};
