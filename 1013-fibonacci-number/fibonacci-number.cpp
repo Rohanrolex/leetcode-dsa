976224738ad05d4e7091618d123ea941cb7c54fb
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2026-02-18 20:08:04 +0530

    Time: 2 ms (78.25%) | Memory: 7.7 MB (64.21%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int prev1 = 0, prev2 = 1, current;
+        for (int i = 2; i <= n; ++i) {
+            current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+        return current;
+    }
+};
