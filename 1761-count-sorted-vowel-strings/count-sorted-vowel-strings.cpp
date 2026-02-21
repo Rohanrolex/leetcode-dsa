c58d22dfedc14834ec8036012d3efb5e8d588c51
Rohan Mondal <163380694+Rohanrolex@users.noreply.github.com>
2026-02-21 10:07:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (30.85%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
-    int solve(int n, int index) {
-        if (n == 0) return 1;
-        if (index == 5) return 0;
-
-        // choose current vowel
-        int take = solve(n - 1, index);
-
-        // move to next vowel
-        int skip = solve(n, index + 1);
-
-        return take + skip;
-    }
+    
 
     int countVowelStrings(int n) {
-        return solve(n, 0);
+      return((n+1)*(n+2)*(n+3)*(n+4))/24;
     }
 };
